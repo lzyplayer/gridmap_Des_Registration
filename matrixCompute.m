53778b6952ef0de506f21a1fb42c8353bc962b91
lzyplayer <292442545@qq.com>
2018-11-15 17:50:32 +0800

    work on

--- matrixCompute.m
+++ matrixCompute.m
@@ -7,6 +7,9 @@ end
 
 % construct the convariance matrix
 K = size(A,2);
-B = bsxfun(@minus, A, xc(:));
+B = bsxfun(@minus, A, xc(:));  % B为所有A指向兴趣点xc的向量
 M = 1/K*(B*B');
 
+% B : 2 * n
+% M = [ mean(x*x) mean(x*y)
+%        mean(x*y) mean(y*y) ]
--- svdCov.m
+++ svdCov.m
@@ -1,7 +1,8 @@
 function [s,n] = svdCov(nnIdx, idx, Data, Seed)
-
-nnPt = Data(:,nnIdx);
+%SVDCOV 第idX号种子点周围有nnIdx里的这些点，用他们来描述种子，
+% Data为所有点的位置信息，seed为为所有种子点位置信息
+nnPt = Data(:,nnIdx); %周围描述点坐标
 C = matrixCompute(nnPt,Seed(:,idx));
 [U,S,~] = svd(C);
-s = diag(S)/sum(diag(S));
+s = diag(S)/sum(diag(S));   %使特征值和唯一
 n = sign(dot(U(:,2),-Seed(:,idx)))*U(:,2);
