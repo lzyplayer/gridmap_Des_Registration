728196aacbfdbe9f6bcbda5cd853577c68f166e0
lzy <lzyplayer@stu.xjtu.edu.cn>
2019-09-06 18:13:45 +0800

    draw_chart

--- demo_resolutionDiff.m
+++ demo_resolutionDiff.m
@@ -1,24 +1,26 @@
+% clear;
+% clc;close all;
+% run('D:\matlab2019\vlfeat-0.9.21\toolbox\vl_setup.m')
+% 
+% addpath('./flann/');
+% addpath('./keypointExtraction/');
+% % addpath('./estimateRigidTransform');
+% s=100;
+% gridStep=0.2;
+% overlap=0.7;
+% icpSteps=100;
+% TrMin=0.7;
+% TrMax=0.8;
+% %尺度平衡子（让待配准的两幅图比例保持又不过多影响gridstep）
+% zoomVar=1;
+% downStep=0.08;
 
-clc;clear;close all;
-% run('C:/Matlab 2018/vlfeat/toolbox/vl_setup.m')
-addpath('./flann/');
-addpath('./keypointExtraction/');
-% addpath('./estimateRigidTransform');
-s=100;
-gridStep=0.1;
-overlap=0.2;
-icpSteps=100;
-TrMin=0.2;
-TrMax=0.8;
-%尺度平衡子（让待配准的两幅图比例保持又不过多影响gridstep）
-zoomVar=1;
-downStep=0.08;
-
-%% 转灰度
-map1=rgb2gray(imread('Fr1_3.png'));
-map2=rgb2gray(imread('Fr2_3.png'));
-map1 = imresize(map1,1.0);
-map2 = imresize(map2,1.3);
+% % 转灰度
+% map1=rgb2gray(imread('..\map_data\pair2\1.png','png'));
+% map2=rgb2gray(imread('..\map_data\pair2\2.png','png'));
+map1 = imresize(ori_map1,1.0);
+% resize_times=1.2;
+map2 = imresize(ori_map2,resize_times);
 % imshowpair(map1,map2);
 tic;
 
@@ -29,71 +31,71 @@ testMap2 = single(map2);
 % imshow(testMap1Ad)
 % imshow(im2single(map1))
 
-%% 选取harris-Laplace特殊点
-figure
-imshow(map1);hold on
-result = vl_covdet(testMap1,'verbose',...
-    'Method','MultiscaleHarris',...
+% % 选取harris-Laplace特殊点
+% figure
+% imshow(map1);hold on,'verbose'
+result = vl_covdet(testMap1,...
+    'Method','HarrisLaplace',...
     'BaseScale',2,...
     'OctaveResolution',3,...
     'DoubleImage',false);
 % T = result(1:2) ;
 % A = reshape(result(3:6),2,2) ;
 % result(3:6,:)=result(3:6,:)*6;
-% sele= result(3,:)>4.5;
-vl_plotframe(result(:,sele));
+sele= result(3,:)>4.5;
+% vl_plotframe(result(:,sele));
 cornersM1=SURFPoints(result(1:2,sele)','Scale',result(3,sele));
 
-figure
-imshow(map2);hold on
-result = vl_covdet(testMap2,'verbose',...
-    'Method','MultiscaleHarris',...
+% figure
+% imshow(map2);hold on,'verbose'
+result = vl_covdet(testMap2,...
+    'Method','HarrisLaplace',...
     'BaseScale',2,...
     'OctaveResolution',3,...
     'DoubleImage',false);
 % T = result(1:2) ;
 % A = reshape(result(3:6),2,2) ;
 sele=result(3,:)>4.5;
-vl_plotframe(result(:,sele));
+% vl_plotframe(result(:,sele));
 cornersM2=SURFPoints(result(1:2,sele)','Scale',result(3,sele));
 
 
 
-%% Laplaceharris(慢matlab)
+% % Laplaceharris(慢matlab)
 %  cornersM1=kp_harrislaplace(map1);
 %  cornersM2=kp_harrislaplace(map2);
 %  draw(map1,cornersM1,'m1');
 %  draw(map2,cornersM2,'m2');
 %  %%%here
 
-%% 选取harris特殊点 
+% % 选取harris特殊点 
 %  cornersM1=detectHarrisFeatures(map1);
 %  cornersM2=detectHarrisFeatures(map2);
 %  
 
-%% FAST角点
+% % FAST角点
 %  cornersM1=detectFASTFeatures(map1);
 %  cornersM2=detectFASTFeatures(map2);
 
-%% 寻找surf和高斯差分极值
+% % 寻找surf和高斯差分极值
 % cornersM1 = detectSURFFeatures(map1);
 % cornersM2 = detectSURFFeatures(map2);
-%% BRISK
+% % BRISK
 % cornersM1 = detectBRISKFeatures(map1);
 % cornersM2 = detectBRISKFeatures(map2);
 
 % 1.选取harris角点最强点200个 
 % 2.均匀分布选取harris角点最强点200个    (current) 
-figure;
-imshow(map1);hold on;
+% figure;
+% imshow(map1);hold on;
 %plot(selectUniform(cornersM1,200,size(map1)));%
 seleCorM1=selectUniform(cornersM1,600,size(map1));% seleCorM1=cornersM1.selectStrongest(300);
-showPoint(seleCorM1.Location);
+% showPoint(seleCorM1.Location);
 
-figure;
-imshow(map2);hold on;
+% figure;
+% imshow(map2);hold on;
 seleCorM2=selectUniform(cornersM2,600,size(map2));
-showPoint(seleCorM2.Location);
+% showPoint(seleCorM2.Location);
 
 %尺度orNot
 zSeleCorM1=SURFPoints(seleCorM1.Location/s,'Scale',seleCorM1.Scale);
@@ -101,7 +103,7 @@ zSeleCorM2=SURFPoints(seleCorM2.Location/s,'Scale',seleCorM2.Scale);
 % zSeleCorM1=SURFPoints(seleCorM1.Location/s);
 % zSeleCorM2=SURFPoints(seleCorM2.Location/s);
 
-%% 提取二维点云
+% % 提取二维点云
 [pcMap3d1,pointCMap1]=exarctPCfroImg(map1);
 [pcMap3d2,pointCMap2]=exarctPCfroImg(map2);
 pointCMap1=pointCMap1/s;
@@ -117,11 +119,11 @@ pointCMap2=pointCMap2/s;
 % plot(zSeleCorM2.Location(:,1),zSeleCorM2.Location(:,2),'.');
 % axis equal
 
-%% 基于尺度获取描述子
+% % 基于尺度获取描述子
 [M1Desp,M1Scale,M1Seed,M1Norm]=exarctEIG2d(pointCMap1,gridStep,zSeleCorM1,zoomVar); 
 [M2Desp,M2Scale,M2Seed,M2Norm]=exarctEIG2d(pointCMap2,gridStep,zSeleCorM2,zoomVar); 
 
-%% 展示描述半径
+% % 展示描述半径
 % figure;
 % plot(pointCMap1(:,1),pointCMap1(:,2),'.');hold on;
 % for i=1:length(M1Seed)
@@ -139,24 +141,24 @@ pointCMap2=pointCMap2/s;
 % end
 % axis equal
 
-%% 匹配
+% % 匹配
 Motion=eigMatch2D(M1Desp,M2Desp,M1Scale,M2Scale,M1Seed,M2Seed,M1Norm,M2Norm,overlap,gridStep,map1,map2,s);
 
-testcode
-close all; plot(s*pointCMap1(:,1),s*pointCMap1(:,2),'.'); hold on plot(s*M1Seed(1,:)',s*M1Seed(2,:)','.','MarkerSize',10 ,'color','red');
+% % testcode
+% % close all; plot(s*pointCMap1(:,1),s*pointCMap1(:,2),'.'); hold on plot(s*M1Seed(1,:)',s*M1Seed(2,:)','.','MarkerSize',10 ,'color','red');
 
-%% 修正尺度 
-% s=SQRT((S*COS(THETA))^2+(S*SIN(THETA))^2)
+% % 修正尺度 
+% % s=SQRT((S*COS(THETA))^2+(S*SIN(THETA))^2)
 %%根据相似变换和比例公式推倒缩放因子
 scaleC=sqrt(sum(Motion(1:2,1).^2));
 pointCMap1Scaled = pointCMap1*scaleC;
 pointCMap1ScaledDown = pcdownsample2d(pointCMap1Scaled,downStep);
 pointCMap2ScaledDown = pcdownsample2d(pointCMap2,downStep);
-%% 后端ICP
+% 后端ICP
 
 R0=Motion(1:2,1:2);
 t0=Motion(1:2,3);
-% opMotion=Motion;
+opMotion=Motion;
 R0=R0./scaleC;%成为刚体变换以ICP
 [R,t]=fastTrICP2D(pointCMap2ScaledDown',pointCMap1ScaledDown',R0,t0,TrMin,TrMax,icpSteps);
 opMotion=[R.*scaleC,t;0 0 1];
---
+++ drawChart.m
@@ -0,0 +1,66 @@
+clc;clear;close all;
+set_times=(1:0.1:6);
+run('D:\matlab2019\vlfeat-0.9.21\toolbox\vl_setup.m')
+addpath('./flann/');
+addpath('./keypointExtraction/');
+% addpath('./estimateRigidTransform');
+s=100;
+gridStep=0.15;
+overlap=0.2;
+icpSteps=100;
+TrMin=0.2;
+TrMax=0.8;
+%尺度平衡子（让待配准的两幅图比例保持又不过多影响gridstep）
+zoomVar=1;
+downStep=0.08;
+% ori_map1=rgb2gray(imread('..\map_data\pair5\loop6_1.png'));
+% ori_map2=rgb2gray(imread('..\map_data\pair5\loop10_1.png'));
+ori_map1  =imread('..\map_data\pair5\loop6_1_zoom.png');
+ori_map2  =imread('..\map_data\pair5\loop10_1.png');
+datasheet_report(1,:)=set_times;
+iter=1;
+for i=set_times
+
+
+    resize_times=i;
+    run("demo_resolutionDiff.m");
+    datasheet_report(2,iter)= abs(scaleC-i);
+    
+    disp([num2str(i),': ',num2str(abs(scaleC-i))]);
+    iter=iter+1;
+end
+
+
+x=datasheet_report(1,:);
+y=datasheet_report(2,:);
+figure;
+handler_c = plot(x,y);
+
+datasheet_report_loop5 = datasheet_report;
+
+%% plot multi line
+
+load('fr_scale.mat')
+load('intel_scale.mat')
+load('loop5_scale.mat')
+figure;
+hold on;
+lineWid = 1;
+line_handler_fr = plot(datasheet_report_fr(1,:),datasheet_report_fr(2,:));
+line_handler_intel = plot(datasheet_report_intel(1,:),datasheet_report_intel(2,:));
+line_handler_loop5 = plot(datasheet_report_loop5(1,:),datasheet_report_loop5(2,:));
+line_handler_fr.LineStyle='--';
+line_handler_intel.LineStyle='-';
+line_handler_loop5.LineStyle='-.';
+line_handler_fr.LineWidth=lineWid;
+line_handler_intel.LineWidth=lineWid-0.3;
+line_handler_loop5.LineWidth=lineWid+0.2;
+grid on;
+currAxis=gca;
+currAxis.XTick=[1:0.5:6];
+currAxis.YTick=[-0.5:0.5:4];
+currAxis.XTickLabel
+axis([1,6,-0.5,4]);
+xlabel('{\fontsize{12}Scal{e_P} : Scal{e_Q}}')    ;
+ylabel('{\fontsize{12}\DeltaScale}')    ;
+legend({'Freiburg, Building 079','Intel Reserach Lab^{[8]}','LSRC, D-Wing^{[10]}'},'Location','northwest')
